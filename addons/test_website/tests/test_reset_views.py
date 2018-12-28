9804cf721ea14c27c1957b2abc925fcff610b9db
Romain Derie <rde@odoo.com>
2018-12-28 16:24:33 +0100

    [FIX] website: restore reset_templates functionality
    Since AST was introduced to compile Qweb templates, the reset templates
    functionality is not working anymore.
    Actually, it does not even appear anymore as the conditions to trigger and show
    the reset view behavior can't be True anymore.
    Indeed, the code is still importing the old QWebException from odoo.exceptions
    which is now just an empty class.
    The QWebException that should now be used is the one from odoo.base.models.qweb
    
    More than just a wrong import, the QWebException properties have completely
    changed.
    
    Since its last working version (Odoo 9.0), a lot of changes occured, mostly:
      - Odoo 11.0: new model website.page, which completely changed the pages
        behavior and changed the models.
      - Odoo 12.0: multi-website, which introduced the Copy On Write (COW) that
        redefined how we write on view in a website context. We now need to reset
        copied/specific views that have no model_data_id.
    
    This fix is for a stable version. There is some flows that can't be fixed
    without a refactoring or new fields (mainly because of the arch_fs being
    erased when writing on `arch`, plus we can't introduce a module reload in
    stable).
    The goal of this commit is to fix the basic and standard case of broken views,
    which is when non-technical users are breaking the views through HTML editor.
    
    To reset the views, we will read the original arch in the XML file, as the
    dev mode is doing.
    To be eligible for the reset, the broken view need an `arch_fs` set. If the
    view arch has been modified anywhere else than the frontend, the `arch_fs`
    will be erased and the view wont be able to be reset.
    As the broken views will be specific views, we will simply read the generic
    view in the XML file and write it on the specific. We can't simply remove the
    specific view as we might be on a specific tree.
    
    Specific case, in case of custom views create dropping a snippet in an oe_struc
    we just delete this inehrited view to allow end user to have their page back.
    
    In some case of bad compilation in sub template, we try to guess which inherited
    view, t-called view is broken by string matching based on 'last_path_node' xpath.
    
    No sense to mark generic view in no_update, when create specific view.
    
    Add new module test_website to add tests, it will be usefull for futur test of
    install/uninstall and ...
    
    Todo in master:
      - try to track on QWebException the real template that is broken and not only
        the last node from the main template.
    
    Co-authored-by: "Romain Derie <rde@odoo.com>"
    Co-authored-by: "Jérémy Kersten <jke@openerp.com>"

---
+++ addons/test_website/tests/test_reset_views.py
@@ -0,0 +1,84 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import odoo.tests
+
+from odoo.tools import mute_logger
+
+
+def break_view(view, fr='<p>placeholder</p>', to='<p t-field="not.exist"/>'):
+    view.arch = view.arch.replace(fr, to)
+
+
+@odoo.tests.common.tagged('post_install', '-at_install')
+class TestWebsiteResetViews(odoo.tests.HttpCase):
+
+    def do_test(self, name):
+        self.browser_js(
+            "/",
+            "odoo.__DEBUG__.services['web_tour.tour'].run('%s')" % name,
+            "odoo.__DEBUG__.services['web_tour.tour'].tours.%s.ready" % name,
+            login="admin"
+        )
+
+    def setUp(self):
+        super(TestWebsiteResetViews, self).setUp()
+
+        self.Website = self.env['website']
+        self.View = self.env['ir.ui.view']
+        self.test_view = self.Website.viewref('test_website.test_view')
+        self.test_page_view = self.Website.viewref('test_website.test_page_view')
+        self.test_view_to_be_t_called = self.Website.viewref('test_website.test_view_to_be_t_called')
+        self.test_view_child_broken = self.Website.viewref('test_website.test_view_child_broken')
+
+    @mute_logger('odoo.addons.website.models.ir_http')
+    def test_reset_specific_page_view(self):
+        total_views = self.View.search_count([('type', '=', 'qweb')])
+        # Trigger COW then break the QWEB XML on it
+        break_view(self.test_page_view.with_context(website_id=1))
+        self.assertEqual(total_views + 1, self.View.search_count([('type', '=', 'qweb')]), "Ensure COW was correctly created")
+        self.do_test('test_reset_specific_page_view')
+
+    @mute_logger('odoo.addons.website.models.ir_http')
+    def test_reset_specific_view_controller(self):
+        total_views = self.View.search_count([('type', '=', 'qweb')])
+        # Trigger COW then break the QWEB XML on it
+        break_view(self.test_view.with_context(website_id=1))
+        self.assertEqual(total_views + 1, self.View.search_count([('type', '=', 'qweb')]), "Ensure COW was correctly created")
+        self.do_test('test_reset_specific_view_controller')
+
+    @mute_logger('odoo.addons.website.models.ir_http')
+    def test_reset_specific_view_controller_t_called(self):
+        total_views = self.View.search_count([('type', '=', 'qweb')])
+        # Trigger COW then break the QWEB XML on it
+        break_view(self.test_view_to_be_t_called.with_context(website_id=1))
+        self.test_view.arch = str.replace(self.test_view.arch, '<p>placeholder</p>', '<t t-call="test_website.test_view_to_be_t_called"/>')
+        self.assertEqual(total_views + 1, self.View.search_count([('type', '=', 'qweb')]), "Ensure COW was correctly created")
+        self.do_test('test_reset_specific_view_controller_t_called')
+
+    @mute_logger('odoo.addons.website.models.ir_http')
+    def test_reset_specific_view_controller_inherit(self):
+        # Activate and break the inherited view
+        self.test_view_child_broken.active = True
+        break_view(self.test_view_child_broken.with_context(website_id=1))
+        self.do_test('test_reset_specific_view_controller_inherit')
+
+    # also mute ir.ui.view as `get_view_id()` will raise "Could not find view object with xml_id 'not.exist'""
+    @mute_logger('odoo.addons.website.models.ir_http', 'odoo.addons.website.models.ir_ui_view')
+    def test_reset_specific_view_controller_broken_request(self):
+        total_views = self.View.search_count([('type', '=', 'qweb')])
+        # Trigger COW then break the QWEB XML on it
+        break_view(self.test_view.with_context(website_id=1), to='<t t-esc="request.env[\'website\'].browse(\'-1\').name" />')
+        self.assertEqual(total_views + 1, self.View.search_count([('type', '=', 'qweb')]), "Ensure COW was correctly created (1)")
+        self.do_test('test_reset_specific_view_controller_broken_request')
+
+    @mute_logger('odoo.addons.website.models.ir_http', 'odoo.addons.website.models.ir_ui_view')
+    def test_reset_specific_view_controller_inexisting_template(self):
+        total_views = self.View.search_count([('type', '=', 'qweb')])
+        # Trigger COW then break the QWEB XML on it
+        break_view(self.test_view.with_context(website_id=1), to='<t t-call="not.exist"/>')
+        self.assertEqual(total_views + 1, self.View.search_count([('type', '=', 'qweb')]), "Ensure COW was correctly created (2)")
+        self.do_test('test_reset_specific_view_controller_inexisting_template')
+
+    @mute_logger('odoo.addons.website.models.ir_http')
+    def test_reset_page_view_complete_flow(self):
+        self.do_test('test_reset_page_view_complete_flow')
