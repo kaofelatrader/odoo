38666bc845480421f5ebfd7cac2801353fd0e3c4
qmo-odoo <qmo@odoo.com>
2019-03-27 10:16:53 +0100

    [IMP] website_slides: Improve methods to get user certificates
    This commit improves a few methods related to the user profiles:
     - Change _prepare_all_users_values so that it
       works on batch
     - Change the _get_user_certificates to avoid having
       a search in a loop
     - Introduce a new method that will only get the number
       of certifications per user

--- addons/website_profile/controllers/main.py
+++ addons/website_profile/controllers/main.py
@@ -16,7 +16,7 @@ from odoo.osv import expression
 class WebsiteProfile(http.Controller):
     _users_per_page = 30
     _pager_max_pages = 5
-    
+
     # Profile
     # ---------------------------------------------------
 
@@ -196,16 +196,19 @@ class WebsiteProfile(http.Controller):
 
     # All Users Page
     # ---------------------------------------------------
-    def _prepare_all_users_values(self, user):
-        return {
-            'position': user.karma_position,
-            'id': user.id,
-            'name': user.name,
-            'company_name': user.company_id.name,
-            'rank': user.rank_id.name,
-            'karma': user.karma,
-            'badge_count': len(user.badge_ids),
-        }
+    def _prepare_all_users_values(self, users):
+        user_values = []
+        for user in users:
+            user_values.append({
+                'position': user.karma_position,
+                'id': user.id,
+                'name': user.name,
+                'company_name': user.company_id.name,
+                'rank': user.rank_id.name,
+                'karma': user.karma,
+                'badge_count': len(user.badge_ids),
+            })
+        return user_values
 
     @http.route(['/profile/users',
                  '/profile/users/page/<int:page>'], type='http', auth="public", website=True)
@@ -224,7 +227,7 @@ class WebsiteProfile(http.Controller):
                                       scope=page_count if page_count < self._pager_max_pages else self._pager_max_pages)
 
         users = User.sudo().search(dom, limit=self._users_per_page, offset=pager['offset'], order='karma DESC')
-        user_values = [self._prepare_all_users_values(user) for user in users]
+        user_values = self._prepare_all_users_values(users)
 
         values = {
             'top3_users': user_values[:3] if not search_term and page == 1 else None,
