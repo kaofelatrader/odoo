shaneebkhan/android-app

[IMP] point_of_sale: Display reason for device deconnection
Display notification showing why the device disconnected, if the reason is specified.

addons/point_of_sale/static/src/js/devices.js
@@ -172,6 +172,13 @@ var ProxyDevice  = core.Class.extend(mixins.PropertiesMixin,{
             });
     },
     set_driver_connection_status: function (status, driver) {
+        if (status && status.reason && status !== this.iot_devices_status[driver] &&
+            ['connected', 'connecting', 'processing'].indexOf(status.value) < 0) {
+            this.pos.chrome.do_notify(
+                _.str.capitalize(driver) + " " + status.value,
+                "Reason: " + status.reason
+            );
+        }
         this.iot_devices_status[driver] = status;
         this.set_connection_status();
     },

kaofelatrader/odoo

addons/point_of_sale/static/src/js/devices.js
@@ -158,6 +158,13 @@ var ProxyDevice  = core.Class.extend(mixins.PropertiesMixin,{
         }
     },
     set_driver_connection_status: function (status, driver) {
+        if (status && status.reason && status !== this.iot_devices_status[driver] &&
+            ['connected', 'connecting', 'processing'].indexOf(status.status) < 0) {
+            this.pos.chrome.do_notify(
+                _.str.capitalize(driver) + " " + status.value,
+                "Reason: " + status.reason
+            );
+        }
         this.iot_devices_status[driver] = status;
         this.set_connection_status();
     },