3e696500aa8cbd9f53c802edf50c0ff8c45a7b57
Ankita Raval <anr@odoo.com>
2019-05-07 16:49:20 +0530

    [IMP] mrp : bom structure report
    Purpose:
     -The BoM Structure report should only list the products and
    quantity but no their cost so that they can be used
    for external use.
    Before commit:
     -Product cost column is visible on bom structure report.
    After commit:
     -Hide the product cost column in report bom structure.
     -Because the BoM Structure report should only list the
    products and quantity but no their cost so that they can be used for external use.
    
    Task Id: 1969709

--- addons/mrp/static/src/js/mrp_bom_report.js
+++ addons/mrp/static/src/js/mrp_bom_report.js
@@ -196,8 +196,6 @@ var MrpBomReport = stock_report_generic.extend({
         this.$('.o_mrp_bom_cost.o_hidden, .o_mrp_prod_cost.o_hidden').toggleClass('o_hidden');
         if (this.given_context.report_type === 'bom_structure') {
             this.$('.o_mrp_bom_cost').toggleClass('o_hidden');
-        }
-        if (this.given_context.report_type === 'bom_cost') {
             this.$('.o_mrp_prod_cost').toggleClass('o_hidden');
         }
     },
